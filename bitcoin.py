34a62821a5788aed8cb366fbe2e4e5f966c2bbdd
Lewis Robbins <lewis@Lewiss-MacBook-Pro.local>
2017-07-21 10:10:55 +0100

    added calculator functionality

--- bitcoin.py
+++ bitcoin.py
@@ -4,9 +4,11 @@ import json, requests, sys, pprint
 
 # Compute the location from the command line
 if len(sys.argv) < 2:
-    print('USAGE: bitcoin.py currency')
+    print('USAGE: bitcoin.py currency [-p] [amount]')
     sys.exit()
-location = ' '.join(sys.argv[1:])
+
+location = sys.argv[1]
+
 
 url = "http://api.coindesk.com/v1/bpi/currentprice/{0}.json".format(location)
 response = requests.get(url)
@@ -15,3 +17,10 @@ response.raise_for_status()
 prices = json.loads(response.text)
 value = prices['bpi'][location.upper()]['rate_float']
 print('Current price of bitcoin is: {:.2f} '.format(value) + location)
+
+if '-p' in sys.argv:
+    index = sys.argv.index('-p')
+    amountBTC = float(sys.argv[index+1])
+    priceInCur = value * amountBTC
+    print('Value of {0} BTC is: {1:.2f} {2}'.format(amountBTC, priceInCur, location))
+
